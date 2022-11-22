92862b6d0cae470cf7648e51492b227df3e0e592
lehuutai0310 <lehuutai0310@gmail.com>
2022-11-22 20:39:28 +0700

    bai_tap

--- src/ss13/bai_tap/StrAscending.java
+++ src/ss13/bai_tap/StrAscending.java
@@ -1,2 +1,34 @@
-package ss13.bai_tap;public class StrAscending {
+package ss13.bai_tap;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class StrAscending {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input str: ");
+        String str = scanner.nextLine();
+
+        LinkedList<Character> max = new LinkedList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            LinkedList<Character> list = new LinkedList<>();
+            list.add(str.charAt(i));
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(j) > list.getLast()) {
+                    list.add(str.charAt(j));
+                }
+            }
+            if (list.size() > max.size()) {
+                max.clear();
+                max.addAll(list);
+            }
+            list.clear();
+        }
+
+        for (char ch : max) {
+            System.out.println(ch);
+        }
+        System.out.println();
+    }
 }
