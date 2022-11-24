a273c8b56d2007dd6be74a624dc8f9ef4d52a744
lehuutai0310 <lehuutai0310@gmail.com>
2022-11-24 20:57:44 +0700

    bai_tap

--- src/ss14/bai_tap/InsertionSort.java
+++ src/ss14/bai_tap/InsertionSort.java
@@ -1,2 +1,19 @@
-package ss14.bai_tap;public class InsertionSort {
+package ss14.bai_tap;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] list =  {7,3,8,5,4};
+
+        for (int i = 1; i < list.length; i++) {
+            int key = list[i];
+            int j;
+            for (j = i - 1; j >= 0 && key < list[j]; j--) {
+                list[j + 1] = list[j];
+            }
+            list[j + 1] = key;
+            System.out.println(Arrays.toString(list));
+        }
+    }
 }
