d8daba3e84874b869fd91dbed0b95eaee045c4af
lehuutai0310 <lehuutai0310@gmail.com>
2022-11-30 11:09:10 +0700

    bai_tap

--- src/ss16/bai_tap/bai1/CountriesMain.java
+++ src/ss16/bai_tap/bai1/CountriesMain.java
@@ -2,22 +2,20 @@ package ss16.bai_tap.bai1;
 
 import java.util.ArrayList;
 
+import java.util.Arrays;
 import java.util.List;
 public class CountriesMain {
-    public static final String FILE="src/ss17/bai_tap_2/CountryFile";
+    public static final String FILE="src/ss16/bai_tap/bai1/CountriesFile";
     public static void main(String[] args) {
         Countries countries=new Countries("VN", "Việt Nam ",1);
-        Countries countries1=new Countries("TQ", "Trung Quốc ",2);
-        Countries countries2=new Countries("HQ", "Hàn Quốc ",3);
-        Countries countries3=new Countries("E", "Anh ",4);
-        Countries countries4=new Countries("USA", "Mỹ",5);
+        Countries countries1=new Countries("L", "Lào ",2);
+        Countries countries2=new Countries("N", "Nhật ",3);
         List<Countries> list= new ArrayList<>();
         list.add(countries);
         list.add(countries1);
         list.add(countries2);
-        list.add(countries3);
-        list.add(countries4);
 
+        
         for (Countries C:list) {
             String line = C.toString();
             FileUtils.writeFile(FILE, line);
